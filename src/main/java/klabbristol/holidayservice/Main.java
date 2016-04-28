eba8da4d16236fe49fbd846e6988e95e3fae23ca
Rafal Kondratowicz <r.kondratowicz@kainos.com>
2016-04-28 19:14:17 +0100

    use port specified in environment variables

--- src/main/java/klabbristol/holidayservice/Main.java
+++ src/main/java/klabbristol/holidayservice/Main.java
@@ -1,9 +1,14 @@
 package klabbristol.holidayservice;
 
+import java.util.Optional;
+
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class Main {
     public static void main(String[] args) {
+        port(Optional.ofNullable(System.getenv("PORT")).map(Integer::parseInt).orElse(4567));
+
         get("/", (req, res) -> "hello");
     }
 }
