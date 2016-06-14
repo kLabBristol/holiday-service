59e1cc70964ba87bf860b5de18e59876822d3853
Rafal <rkondratowicz@users.noreply.github.com>
2016-06-14 16:01:10 +0200

    non blockin IO

--- src/main/java/klabbristol/holidayservice/handlers/HolidaysHandler.java
+++ src/main/java/klabbristol/holidayservice/handlers/HolidaysHandler.java
@@ -2,12 +2,11 @@ package klabbristol.holidayservice.handlers;
 
 
 import klabbristol.holidayservice.dao.HolidayRepo;
-import klabbristol.holidayservice.model.Holiday;
 import klabbristol.holidayservice.model.NewHoliday;
+import ratpack.exec.Blocking;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
 
-import java.util.List;
 import java.util.Optional;
 
 import static ratpack.jackson.Jackson.fromJson;
@@ -22,17 +21,18 @@ public class HolidaysHandler implements Handler {
             .post(() -> {
                 ctx.parse(fromJson(NewHoliday.class))
                     .then(holiday -> {
-                        repo.add(holiday);
-                        ctx.getResponse().status(201).send();
+                        Blocking
+                            .op(() -> repo.add(holiday))
+                            .then(() -> ctx.getResponse().status(201).send());
                     });
             })
             .get(() -> {
-                List<Holiday> holidays =
-                    Optional.ofNullable(ctx.getRequest().getQueryParams().get("user"))
+                Blocking
+                    .get(() -> Optional.ofNullable(ctx.getRequest().getQueryParams().get("user"))
                         .map(repo::findByUser)
-                        .orElse(repo.findAll());
-
-                ctx.render(json(holidays));
+                        .orElse(repo.findAll())
+                    )
+                    .then(holidays -> ctx.render(json(holidays)));
             })
         );
     }
